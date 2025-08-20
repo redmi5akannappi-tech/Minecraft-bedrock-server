3a572552b81790e5886ed74434a006d3a58cbdeb
collegepurpose096-blip <collegepurpose096@gmail.com>
2025-08-20 15:13:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-cd /tmp/server
-
-# Accept EULA if missing
-if [ ! -f "eula.txt" ]; then
-    echo "eula=true" > eula.txt
-fi
-
+set -e
 echo "Starting Minecraft Bedrock server..."
 LD_LIBRARY_PATH=. ./bedrock_server
