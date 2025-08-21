962ecfcf40b103fb178e322ba7cf51d064c22922
collegepurpose096-blip <collegepurpose096@gmail.com>
2025-08-21 10:03:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,12 @@ set -e
 ./install_playit.sh &
 PLAYIT_PID=$!
 
-# Start Bedrock server (foreground)
-./start.sh
+# Keep restarting Bedrock to avoid memory leaks
+while true; do
+    ./start.sh
+    echo "Bedrock server stopped or crashed. Restarting in 10 seconds..."
+    sleep 10
+done
 
-# Clean up Playit if Bedrock exits
+# Cleanup Playit when container stops
 kill $PLAYIT_PID
