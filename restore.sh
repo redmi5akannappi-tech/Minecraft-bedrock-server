510c2e989da0df3c954d727a4fc8c27163086635
redmi-derosky <redmi5akannappi@gmail.com>
2025-08-27 19:22:36 +0530

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -6,7 +6,7 @@ RESTORE_DIR="/tmp/bedrock_restore"
 WORLD_DIR="/server/worlds"
 
 rm -rf "$RESTORE_DIR"
-git clone https://github.com/redmi5akannappi-tech/Minecraft-world-data.git "$RESTORE_DIR"
+git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/redmi5akannappi-tech/Minecraft-world-data.git "$RESTORE_DIR"
 
 cd "$RESTORE_DIR"
 cat world_chunk_* > world_backup.tar.gz
@@ -14,5 +14,7 @@ cat world_chunk_* > world_backup.tar.gz
 mkdir -p "$WORLD_DIR"
 tar xzf world_backup.tar.gz -C "$WORLD_DIR"
 
-# Ensure server.properties points to correct world
-sed -i "s/^level-name=.*/level-name=$WORLD_NAME/" /server/server.properties
+# Optional: Update server.properties if it exists
+if [ -f "/server/server.properties" ]; then
+    sed -i "s/^level-name=.*/level-name=$WORLD_NAME/" /server/server.properties
+fi
